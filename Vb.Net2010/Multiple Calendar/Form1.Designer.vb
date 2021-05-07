e441c713e311d04c817b5b749ab24df08a85cfc1
Viscomsoft <support@viscomsoft.com>
2021-05-07 10:22:56 +0800

    Add files via upload

---
+++ Vb.Net2010/Multiple Calendar/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxCalendar1 = New AxCALENDARLib.AxCalendar()
+        Me.AxCalendar2 = New AxCALENDARLib.AxCalendar()
+        Me.AxCalendar3 = New AxCALENDARLib.AxCalendar()
+        Me.AxCalendar4 = New AxCALENDARLib.AxCalendar()
+        Me.AxCalendar5 = New AxCALENDARLib.AxCalendar()
+        Me.AxCalendar6 = New AxCALENDARLib.AxCalendar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.AxCalendar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxCalendar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxCalendar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxCalendar4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxCalendar5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxCalendar6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxCalendar1
+        '
+        Me.AxCalendar1.Enabled = True
+        Me.AxCalendar1.Location = New System.Drawing.Point(1, 42)
+        Me.AxCalendar1.Name = "AxCalendar1"
+        Me.AxCalendar1.OcxState = CType(resources.GetObject("AxCalendar1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxCalendar1.Size = New System.Drawing.Size(391, 306)
+        Me.AxCalendar1.TabIndex = 0
+        '
+        'AxCalendar2
+        '
+        Me.AxCalendar2.Enabled = True
+        Me.AxCalendar2.Location = New System.Drawing.Point(398, 42)
+        Me.AxCalendar2.Name = "AxCalendar2"
+        Me.AxCalendar2.OcxState = CType(resources.GetObject("AxCalendar2.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxCalendar2.Size = New System.Drawing.Size(391, 306)
+        Me.AxCalendar2.TabIndex = 1
+        '
+        'AxCalendar3
+        '
+        Me.AxCalendar3.Enabled = True
+        Me.AxCalendar3.Location = New System.Drawing.Point(795, 42)
+        Me.AxCalendar3.Name = "AxCalendar3"
+        Me.AxCalendar3.OcxState = CType(resources.GetObject("AxCalendar3.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxCalendar3.Size = New System.Drawing.Size(391, 306)
+        Me.AxCalendar3.TabIndex = 2
+        '
+        'AxCalendar4
+        '
+        Me.AxCalendar4.Enabled = True
+        Me.AxCalendar4.Location = New System.Drawing.Point(1, 354)
+        Me.AxCalendar4.Name = "AxCalendar4"
+        Me.AxCalendar4.OcxState = CType(resources.GetObject("AxCalendar4.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxCalendar4.Size = New System.Drawing.Size(391, 306)
+        Me.AxCalendar4.TabIndex = 3
+        '
+        'AxCalendar5
+        '
+        Me.AxCalendar5.Enabled = True
+        Me.AxCalendar5.Location = New System.Drawing.Point(398, 354)
+        Me.AxCalendar5.Name = "AxCalendar5"
+        Me.AxCalendar5.OcxState = CType(resources.GetObject("AxCalendar5.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxCalendar5.Size = New System.Drawing.Size(391, 306)
+        Me.AxCalendar5.TabIndex = 4
+        '
+        'AxCalendar6
+        '
+        Me.AxCalendar6.Enabled = True
+        Me.AxCalendar6.Location = New System.Drawing.Point(795, 354)
+        Me.AxCalendar6.Name = "AxCalendar6"
+        Me.AxCalendar6.OcxState = CType(resources.GetObject("AxCalendar6.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxCalendar6.Size = New System.Drawing.Size(391, 306)
+        Me.AxCalendar6.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(21, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(204, 25)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Highlight Importance days"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1214, 680)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.AxCalendar6)
+        Me.Controls.Add(Me.AxCalendar5)
+        Me.Controls.Add(Me.AxCalendar4)
+        Me.Controls.Add(Me.AxCalendar3)
+        Me.Controls.Add(Me.AxCalendar2)
+        Me.Controls.Add(Me.AxCalendar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxCalendar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxCalendar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxCalendar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxCalendar4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxCalendar5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxCalendar6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents AxCalendar1 As AxCALENDARLib.AxCalendar
+    Friend WithEvents AxCalendar2 As AxCALENDARLib.AxCalendar
+    Friend WithEvents AxCalendar3 As AxCALENDARLib.AxCalendar
+    Friend WithEvents AxCalendar4 As AxCALENDARLib.AxCalendar
+    Friend WithEvents AxCalendar5 As AxCALENDARLib.AxCalendar
+    Friend WithEvents AxCalendar6 As AxCALENDARLib.AxCalendar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Vb.Net2010/Multiple Calendar/Form1.vb
@@ -0,0 +1,113 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        AxCalendar1.Month = 1
+        AxCalendar2.Month = 2
+        AxCalendar3.Month = 3
+        AxCalendar4.Month = 4
+        AxCalendar5.Month = 5
+        AxCalendar6.Month = 6
+
+
+
+
+    End Sub
+
+    Public Function Color2Uint32(ByVal clr As Color) As UInt32
+        Dim t As Int32
+        Dim a() As Byte
+
+        t = ColorTranslator.ToOle(clr)
+
+
+        a = BitConverter.GetBytes(t)
+
+        Return BitConverter.ToUInt32(a, 0)
+
+
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim i As Object
+        Dim clrColor As Color
+
+        clrColor = Color.FromArgb(128, 255, 255)
+        Me.AxCalendar1.Month = Me.AxCalendar1.Month
+        Me.AxCalendar1.Year = Me.AxCalendar1.Year
+
+        Me.AxCalendar1.SetText(1, "New Year")
+
+        For i = 1 To 10
+
+            Me.AxCalendar1.DayTextColor = System.Drawing.Color.Red
+            Me.AxCalendar1.SetHighLightDay(AxCalendar1.Year, AxCalendar1.Month, i, Color2Uint32(clrColor))
+
+        Next
+
+        Me.AxCalendar1.Redraw()
+
+        'calendar 2
+        clrColor = Color.FromArgb(255, 128, 100)
+
+        Me.AxCalendar2.Month = Me.AxCalendar2.Month
+        Me.AxCalendar2.Year = Me.AxCalendar2.Year
+
+        For i = 10 To 15
+            Me.AxCalendar2.SetHighLightDay(AxCalendar2.Year, AxCalendar2.Month, i, Color2Uint32(clrColor))
+        Next
+
+        Me.AxCalendar2.Redraw()
+
+        'calendar 3
+        clrColor = Color.FromArgb(200, 50, 100)
+
+        Me.AxCalendar3.Month = Me.AxCalendar3.Month
+        Me.AxCalendar3.Year = Me.AxCalendar3.Year
+
+        For i = 20 To 28
+            Me.AxCalendar3.SetHighLightDay(AxCalendar3.Year, AxCalendar3.Month, i, Color2Uint32(clrColor))
+        Next
+
+        Me.AxCalendar3.Redraw()
+
+        'calendar 4
+        clrColor = Color.FromArgb(100, 255, 100)
+
+        Me.AxCalendar4.Month = Me.AxCalendar4.Month
+        Me.AxCalendar4.Year = Me.AxCalendar4.Year
+
+        Me.AxCalendar4.DayTextFontSize = 8
+        Me.AxCalendar4.SetText(1, "Anniversary")
+        For i = 1 To 5
+            Me.AxCalendar4.SetHighLightDay(AxCalendar4.Year, AxCalendar4.Month, i, Color2Uint32(clrColor))
+        Next
+
+        Me.AxCalendar4.Redraw()
+
+        'calendar 5
+        clrColor = Color.FromArgb(100, 155, 255)
+
+        Me.AxCalendar5.Month = Me.AxCalendar5.Month
+        Me.AxCalendar5.Year = Me.AxCalendar5.Year
+
+
+        For i = 1 To 10 Step 2
+            Me.AxCalendar5.SetHighLightDay(AxCalendar5.Year, AxCalendar5.Month, i, Color2Uint32(clrColor))
+        Next
+
+        Me.AxCalendar5.Redraw()
+
+        'calendar 6
+        clrColor = Color.FromArgb(200, 100, 255)
+
+        Me.AxCalendar6.Month = Me.AxCalendar6.Month
+        Me.AxCalendar6.Year = Me.AxCalendar6.Year
+
+
+        For i = 1 To 30 Step 5
+            Me.AxCalendar6.SetHighLightDay(AxCalendar6.Year, AxCalendar6.Month, i, Color2Uint32(clrColor))
+        Next
+
+        Me.AxCalendar6.Redraw()
+    End Sub
+End Class
